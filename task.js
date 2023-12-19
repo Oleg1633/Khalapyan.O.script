5870af6459599f78cc262ad38571822b28997383
Oleg <oleg345567@gmail.com>
2023-12-19 20:55:19 +0300

    год рождения

--- task.js
+++ task.js
@@ -1,1 +1,7 @@
 console.log('Скрипт работает');
+function calc_age(year_of_birth) {
+    let dt = new Date();
+    var result = dt.getFullYear() - year_of_birth;
+    age.innerHTML = result;
+	return result;
+}
